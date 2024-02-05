c19f0881af7db4b5090637bcbae108fa08b0b9b7
AMITPYT <amitkumar171117@gmail.com>
2024-02-05 15:09:03 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Counter from "./components/FirstPro/features/Counter";
+
+function App() {
+  return (
+    <main className="App">
+      <Counter />
+    </main>
+  );
+}
+
+export default App;
